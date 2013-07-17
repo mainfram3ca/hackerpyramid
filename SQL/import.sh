75a5a7380acf09ed909784b82da628daa2532b22
root <root@hackerpyramid.(none)>
2013-07-17 12:55:50 -0700

    Minor changes for the netbook

--- SQL/import.sh
+++ SQL/import.sh
@@ -4,4 +4,4 @@ USER=`grep '$dbuser=' ../config.php | cut -d\" -f 2`
 DB=`grep '$db=' ../config.php | cut -d\" -f 2`
 PASS=`grep '$dbpass=' ../config.php | cut -d\" -f 2`
 
-mysqldump -u $USER -p$PASS $DB > dump.sql
+mysql -u $USER -p$PASS $DB < dump.sql
