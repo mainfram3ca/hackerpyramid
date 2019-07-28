315ef9d8cd10c7e581c607ba569fed6fbd2ff3c8
allansto <allan.sto@gmail.com>
2019-07-28 17:44:01 -0400

    added scaling routine

---
+++ NewHackerPyramid/assets/rend.js
@@ -0,0 +1,29 @@
+        var $el = $("#very-specific-design");
+        var elHeight = $el.outerHeight();
+        var elWidth = $el.outerWidth();
+
+        var $wrapper = $("#scaleable-wrapper");
+
+        $wrapper.resizable({ resize: doResize });
+
+        function doResize(event, ui) {
+                var scale, origin;
+
+                scale = Math.min(
+                        ui.size.width / elWidth,
+                        ui.size.height / elHeight
+                );
+
+                $el.css({
+                        transform: "translate(-50%, -50%) " + "scale(" + scale + ")" }
+                );
+        }
+
+        var starterData = {
+                size: {
+                        width:  $wrapper.width(),
+                        height: $wrapper.height()
+                }
+        };
+
+        doResize(null, starterData);
